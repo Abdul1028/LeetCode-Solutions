2db64ee492b2c25b30e27218bf5a6dfd50b8c758
Abdul Shaikh <rasoolas2003@gmail.com>
2025-09-13 10:44:28 +0530

    Time: 3 ms (65.94%) | Memory: 17.9 MB (20.04%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -4,28 +4,10 @@ class Solution:
         vowelsSet = ["a","e","i","o","u"] 
         vowels = [ val for key, val in c.items() if key in vowelsSet] 
         cons = [ val for key, val in c.items() if key not in vowelsSet]      
-
-        # for key, val in c.items():
-        #     if key in vowelsSet:
-        #         vowels.append(val)
-        #     else:
-        #         cons.append(val)
-
+        
         if not vowels:
             return max(cons)
         elif not cons:
             return max(vowels)
         else:
             return max(cons) + max(vowels)
-
-        
-        # if vowels:
-        #     a = max(vowels)
-        # else:
-        #     a = 0
-        # if cons:
-        #     b = max(cons)
-        # else:
-        #     b = 0
-        # return a + b
-         
